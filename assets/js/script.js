3955b6bcea6ad4f4b629225e39d1b33180f94835
Nainoa Dinson <nd808@msn.com>
2023-10-17 16:29:49 -1000

    created recipe card in html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
-console.log("hello world")
+
+var moreInfoEl = document.getElementById("more-info");
+
+function showInfo() {
+ 
+  moreInfoEl.removeAttribute("hidden");
+}
+
+document.getElementById("infoButton").addEventListener("click", function() {
+   showInfo();
+
+}); 
