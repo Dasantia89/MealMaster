99cd1bf8cd75e46443255bfedc4ae487cc8efc8f
Nainoa Dinson <nd808@msn.com>
2023-10-19 09:33:43 -1000

    removed card container in html

--- assets/js/display-recipeSearch.js
+++ assets/js/display-recipeSearch.js
@@ -1,56 +1,59 @@
-// APi to display recipe search results
+// API to display recipe search results
 var searchBtn2El = document.querySelector("#searchButton2")
 var recipeContainerEl = document.querySelector(".recipe-container")
-
 let searchQuery = ""
 
+var baseURL = `https://api.spoonacular.com/recipes/complexSearch?apiKey=251762f82c2f4947978e9c9e7007612f&query=pasta&ingredients=&number=20`
+
+  // Function not working. still needs to be fixed
 searchBtn2El.addEventListener("click", (e) => {
   e.preventDefault();
   searchQuery = e.target.querySelector("#query").value;
   console.log(searchQuery)
 })
-console.log(searchQuery)
 
-function getRecipeApi() {
+async function fetchRecipeApi() {
 
-  var requestURL = "https://api.spoonacular.com/recipes/complexSearch?apiKey=251762f82c2f4947978e9c9e7007612f&query=pasta&ingredients=number=16"
+  var baseURL = `https://api.spoonacular.com/recipes/complexSearch?apiKey=251762f82c2f4947978e9c9e7007612f&query=pasta&ingredients=&number=20`
+  var response = await fetch(baseURL)
 
-  fetch(requestURL)
-    .then(function (response) {
-      console.log(response)
-      return response.json();
-    })
-    .then(function (data) {
-      for (var i = 0; i< data.length; i++) {
-        
-      }
-    });
+  console.log(response);
+
+  var data = await response.json();
+  displayRecipe(data.results)
+
+  console.log(data);
 
 }
 
-// getRecipeApi();
+fetchRecipeApi();
 
 // Function to display recipe search results on cards
 
-function displayRecipe(resultObj) {
-  console.log(resultObj)
+function displayRecipe(results) {
+  console.log(results)
 
-  var recipeCard = document.createElement("div")
-  recipeCard.classList.add("card")
+  let generatedRecipe = ""
+  results.map(results => {
+    generatedRecipe =+
+    ` var recipeCard = document.createElement("div")
+      recipeCard.classList.add("card")
 
-  var recipeBody = document.createElement("div")
-  recipeBody.classList.add("card-body")
-  recipeCard.append(recipeBody);
+      var recipeBody = document.createElement("div")
+      recipeBody.classList.add("card-body")
+      recipeCard.append(recipeBody);
   
-  var recipeTitleEl = document.createElement("h5")
-  recipeTitleEl.textContent = resultObj.title;
-  recipeBody.append(recipeTitleEl);
+      var recipeTitleEl = document.createElement("h5")
+      recipeTitleEl.textContent = results.title;
+      recipeBody.append(recipeTitleEl);
 
-  var recipeImgEl = document.createElement("img")
-  recipyBody.append(recipeImgEl);
+      var recipeImgEl = document.createElement("img")
+      recipyBody.append(recipeImgEl);
 
-  var recipeDescriptionEl = document.createElement("div")
-  recipeBody.append(recipeDescriptionEl);
+      var recipeDescriptionEl = document.createElement("div")
+      recipeBody.append(recipeDescriptionEl);
+    `
+})
 
 }
 
