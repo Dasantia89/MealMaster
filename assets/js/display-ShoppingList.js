1a2d0879ae0b115fc5aec884f6d0fa819a081e4e
Dasantia89 <143134358+Dasantia89@users.noreply.github.com>
2023-10-20 13:18:09 -0400

    shopping list is now functional

--- assets/js/display-ShoppingList.js
+++ assets/js/display-ShoppingList.js
@@ -1,7 +1,7 @@
-// var shoppingList = JSON.parse(localStorage.getItem("shoppingList"));
-var apiKey = '78edd369fcca4142972acee0f4fa2117';
+// 1d730a9aef3546fcafa63496f2f8dd33 ea76c6ba131246a798c43a6d36117dca 35ed7a68b3de4009b425e9690978834b
+var apiKey = '1d730a9aef3546fcafa63496f2f8dd33';
 var link = `https://api.spoonacular.com/recipes/informationBulk?includeNutrition=False&apiKey=${apiKey}&ids=`;
-var shoppingList = ['715538', '716429'];
+var shoppingList = JSON.parse(localStorage.getItem("shoppingList")) || [];
 for (x = 0; x < shoppingList.length; x++) {
     link = link + shoppingList[x] + ','
 }
@@ -17,13 +17,23 @@ fetch(link)
     });
 
 function displayShoppingList(data) {
-    // for(var x=0;x<data.length;x++){
-    console.log(data[0]);
-    console.log(data[0].extendedIngredients);
-    console.log(data[0].extendedIngredients[0].name)
+    for (var y = 0; y < data.length; y++) {
+        var card = $(`<div class="card col-3 d-flex flex-column mt-4 mx-2" data-id="${data[y].id}">
+    <img class="border-bottom border-3 border-dark" src="${data[y].image}"></img>
+    <h3 class="p-1 bg-primary text-light card-title border-bottom border-3 border-dark">${data[y].title}</h3>
+    <h4 class="p-1 card-title">Ingredients:</h4>
+    </div>`);
+        var ingredients = $('<ul class="list-group"></ul>')
+        for (var x in data[y].extendedIngredients) {
+            var currentIngredient = data[y].extendedIngredients[x];
+            var measure = ' ' + currentIngredient.measures.us.amount + ' ' + currentIngredient.measures.us.unitShort;
+            var ingredient = `<li class="list-group-item d-flex">
+        <p class="fs-4">${currentIngredient.name}${measure}</p></li>`;
+            ingredients.append(ingredient);
+        }
+        card.append(ingredients);
 
-
-    // }
-    var card = $(`<div class="card"></div>`);
-    
+        $('#list').append(card);
+    }
 }
+
