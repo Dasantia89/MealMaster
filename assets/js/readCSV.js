1facce0d678cc62e4ec4bce169d1645382f19f4b
Nainoa Dinson <nd808@msn.com>
2023-10-20 11:54:12 -1000

    moved current work into script.js

--- assets/js/readCSV.js
+++ assets/js/readCSV.js
@@ -283,3 +283,26 @@ document.addEventListener("DOMContentLoaded", function () {
   loadFavoritesFromLocalStorage();
 });
 
+
+
+
+// API for recipe information
+
+function viewRecipeInfo(results) {
+
+  var recipeId = "716429"
+  var apiKey = '35ed7a68b3de4009b425e9690978834b'
+  var apiURL = `https://api.spoonacular.com/recipes/${recipeId}/information?apiKey=${apiKey}`
+  
+  fetch(apiURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      displayRecipeInfo(data.results)
+      console.log(data);
+    })
+}
+
+
+
